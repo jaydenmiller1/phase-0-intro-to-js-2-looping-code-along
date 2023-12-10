c0ad5e142b847d290a05935f3b6462e1de699939
Jayden Miller <jmillli1213@gmail.com>
2023-12-10 18:41:08 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solutions in this file
+
+
+function writeCards(namesArray, eventName) {
+    
+    let messagesArray = [];
+
+  
+    for (let i = 0; i < namesArray.length; i++) {
+    
+        let message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+
+   
+        messagesArray.push(message);
+    }
+
+ 
+    return messagesArray;
+}
+
+let result = writeCards(["Charlie", "Samip", "Ali"], "birthday");
+console.log(result);
+
+
+
+
+
+function countDown(startingNumber) {
+  
+    let count = startingNumber;
+
+
+    while (count >= 0) {
+        
+        console.log(count);
+
+    
+        count--;
+    }
+}
+
+
+countDown(10);
